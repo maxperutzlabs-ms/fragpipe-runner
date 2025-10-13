2250a9c3c2bb9b55c11da71252fd5774897bc68b
David M. Hollenstein <hollenstein.david@gmail.com>
2025-10-13 15:31:06 +0200

    feat: Add module for working with manifest files

--- src/fragpipe_runner/__init__.py
+++ src/fragpipe_runner/__init__.py
@@ -1,3 +1,4 @@
 from fragpipe_runner.execute import run_fragpipe
+from fragpipe_runner.manifest import sdrf_to_manifest, update_rawfile_paths_in_manifest
 
-__all__ = ["run_fragpipe"]
+__all__ = ["run_fragpipe", "sdrf_to_manifest", "update_rawfile_paths_in_manifest"]
